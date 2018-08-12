3ee3a31f615aabde5d1671bc8207e7ae18b2b4e1
Ikechi Michael <mykehell123@gmail.com>
2018-08-12 14:57:12 +0100

    feat: add bash script to generate code documentation

---
+++ generate-docs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ ! -f ./vendor/bin/phpDocumentor.phar ]
+then
+    curl https://www.phpdoc.org/phpDocumentor.phar --output ./vendor/bin/phpDocumentor.phar
+fi
+
+php ./vendor/bin/phpDocumentor.phar -d ./src/ -t generated-docs  --template=xml
+
+./vendor/bin/phpdocmd generated-docs/structure.xml ./docs
