6de64a6d6b061a7d2b2f63dbaeef7a7fef451dfb
Ikechi Michael <mykehell123@gmail.com>
2018-08-25 10:44:55 -0400

    feat: create-content method

--- src/Traits/ContentableTrait.php
+++ src/Traits/ContentableTrait.php
@@ -3,6 +3,7 @@
 namespace CollegePortal\Models\Traits;
 
 use CollegePortal\Models\Content;
+use CollegePortal\Models\ContentType;
 
 trait ContentableTrait
 {
@@ -16,6 +17,41 @@ trait ContentableTrait
         return $this->hasMany(Content::class, 'owner_id')->whereHas('type', function ($q) {
             return $q->where('type', static::class)->whereNull('related_to');
         });
+    }
+
+    /**
+     * Adds new Content
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\CollegePortal\Models\Content
+     */
+    public function createContent($name, $value)
+    {
+        $type = ContentType::where('name', $name)->firstOrFail();
+        if ($type->format == ContentType::ARRAY) {
+            if (!$this->contents()->where('content_type_id', $type->id)->where('value', $value)->exists()) {
+                return $this->contents()->create([
+                    'content_type_id', $type->id,
+                    'value' => $value
+                ]);
+            }
+            else {
+                $this->contents()->where('content_type_id', $type->id)->where('value', $value)->firstOrFail();
+            }
+        }
+        else {
+            if (!$this->contents()->where('content_type_id', $type->id)->exists()) {
+                return $this->contents()->create([
+                    'content_type_id', $type->id,
+                    'value' => $value
+                ]);
+            }
+            else {
+                $content = $this->contents()->where('content_type_id', $type->id)->firstOrFail();
+                $content->value = $value;
+                $content->save();
+                return $content;
+            }
+        }
     }
 
     public static function boot()
